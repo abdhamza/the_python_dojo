83c61b471a52f88a867ef0f71758adb99d41e0f8
engr.abdullah.hamza@gmail.com <engr.abdullah.hamza@gmail.com>
2024-02-23 09:39:14 +0500

    Loops Question added

--- nested-for-loop/nested_for_loop_15.py
+++ nested-for-loop/nested_for_loop_15.py
@@ -1,5 +1,11 @@
 '''Question 15: Staircase Pattern
 Print a staircase pattern of asterisks (*), where each level has one more asterisk than the previous level, up to 5 levels.'''
+# *
+# **
+# ***
+# ****
+# *****
 
-for i in range(1, 6):
-    print(' ' * (5 - i) + '* ' * i)
+levels = 5
+for level in range(1, levels + 1):
+    print('*' * level)
